387899b84c94f89d6746711de1a4bed9daf55935
judygab <ivditigabeskiria@gmail.com>
2024-07-03 10:47:39 -0400

    update variables classname

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -4,11 +4,11 @@ import { Button } from "@/components/ui/button";
 
 export const Widget = () => {
   return (
-    <div id="widget">
+    <div className="widget">
       <style>{styles}</style>
       <style>{tailwindStyles}</style>
       <div className="widget">Widget</div>
-      <Button className="bg-slate-800">Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 };
